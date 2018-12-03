71afe51ab2ac48f856230e3016c70cc87143d11c
fhenry001 <37307608+fhenry001@users.noreply.github.com>
2018-12-03 15:11:03 +0100

    Info

--- Accueil/Info.Designer.cs
+++ Accueil/Info.Designer.cs
@@ -28,8 +28,12 @@
         /// </summary>
         private void InitializeComponent()
         {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Info));
             this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.lblTitre = new System.Windows.Forms.Label();
             this.label1 = new System.Windows.Forms.Label();
+            this.lblCliente = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             this.SuspendLayout();
             // 
@@ -41,21 +45,53 @@
             this.pictureBox1.TabIndex = 0;
             this.pictureBox1.TabStop = false;
             // 
+            // lblTitre
+            // 
+            this.lblTitre.AutoSize = true;
+            this.lblTitre.Font = new System.Drawing.Font("Microsoft Sans Serif", 20F);
+            this.lblTitre.Location = new System.Drawing.Point(116, 41);
+            this.lblTitre.Name = "lblTitre";
+            this.lblTitre.Size = new System.Drawing.Size(298, 31);
+            this.lblTitre.TabIndex = 1;
+            this.lblTitre.Text = "Qu\'est-ce que IHandy ?";
+            // 
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(210, 36);
+            this.label1.Location = new System.Drawing.Point(107, 128);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(35, 13);
-            this.label1.TabIndex = 1;
-            this.label1.Text = "label1";
+            this.label1.Size = new System.Drawing.Size(333, 65);
+            this.label1.TabIndex = 2;
+            this.label1.Text = resources.GetString("label1.Text");
+            // 
+            // lblCliente
+            // 
+            this.lblCliente.AutoSize = true;
+            this.lblCliente.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblCliente.Location = new System.Drawing.Point(230, 211);
+            this.lblCliente.Name = "lblCliente";
+            this.lblCliente.Size = new System.Drawing.Size(46, 13);
+            this.lblCliente.TabIndex = 3;
+            this.lblCliente.Text = "Cliente";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(213, 237);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(82, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Laetitia Lérandy";
             // 
             // Info
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(524, 469);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.lblCliente);
             this.Controls.Add(this.label1);
+            this.Controls.Add(this.lblTitre);
             this.Controls.Add(this.pictureBox1);
             this.Name = "Info";
             this.Text = "Info";
@@ -68,6 +104,9 @@
         #endregion
 
         private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label lblTitre;
         private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label lblCliente;
+        private System.Windows.Forms.Label label2;
     }
 }
